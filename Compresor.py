65f047374cd700f49144f5420f6253bf7a276e87
Julio-Olascoaga <julio.olascoaga05@gmail.com>
2024-11-11 20:13:14 -0600

    Add files via upload

---
+++ Compresor.py
@@ -0,0 +1,12 @@
+def compress_string(word):
+    comp = ""
+    i = 0
+    
+    while i < len(word):
+        count = 1
+        while i + count < len(word) and word[i] == word[i + count] and count < 9:
+            count += 1
+        comp += str(count) + word[i]
+        i += count
+    
+    return comp
